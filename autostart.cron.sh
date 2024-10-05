16129072fbde07ad76e63c2378cbe459ed12b8b1
Sander Johansen <root@ip-172-31-43-233.us-east-2.compute.internal>
2024-10-05 17:50:18 +0000

    fixed a con bug starting services

--- autostart.cron.sh
+++ autostart.cron.sh
@@ -5,18 +5,18 @@
 #cp -a c9/ /var/www 
 cd /var/www/
 if $(ps aux | grep -q "server.js") ; then
-  echo "found js" ;
-else
   sudo -u www-data -s node server.js -p 8081 -a c9:agurk666&
+else
+  echo "found js" ;
 fi
 if $(ps aux | grep -q "backendserver.py") ; then
-  echo "found py" ;
-else
   cd /var/www/rockefellerthrust/
   sudo -u www-data -s .venv/bin/python /var/www/*/backendserver.py&
   #/var/www/*/backendserver.py
   chown www-data:www-data /var/www/*/uploads/*
   chmod gu+wr /var/www/*/uploads/*
+else
+  echo "found py" ;
 fi
 
 apt-get -y dist-upgrade
