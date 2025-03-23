3d44a515197b1a45258e781c25f1e12067b54ff0
Sander Johansen <sanderm@gmail.com>
2025-03-23 03:07:43 +0000

    fixed visitors count bug from never weekly to all

--- count.sh
+++ count.sh
@@ -1,3 +1,5 @@
+# copy to /etc/cron.hourly/ change www dir:
+cd /var/www/rockefellerthrust/
 (zcat /var/log/apache2/access.log.*.gz; cat /var/log/apache2/access.log.1 /var/log/apache2/access.log)| awk '{print $7 "," $4 "," $1 "," $12}'| sort |egrep "^/" | grep jquery | wc -l > uniq.txt
 (zcat /var/log/apache2/access.log.*.gz; cat /var/log/apache2/access.log.1 /var/log/apache2/access.log)| awk '{print $7 "," $4 "," $1 "," $12}'| sort |egrep "^/" | wc -l > pages.txt
 echo "textpage, textdate, textip, textbrowserversion" > uploads/uniq.csv; (zcat /var/log/apache2/access.log.*.gz; cat /var/log/apache2/access.log.1 /var/log/apache2/access.log)| awk '{print $7 "," $4 "," $1 "," $12}'| sort |egrep "^/" | grep jquery >> uploads/uniq.csv
